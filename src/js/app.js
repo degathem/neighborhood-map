9246772c7a30e3b87cd492919ca63f2c290460cb
Cam Gillespie <gillespie.cam@gmail.com>
2015-03-29 22:55:38 -0600

    Info windows are displaying nowInfo windows are displaying nowInfo windows are displaying nowInfo windows are displaying nowInfo windows are displaying nowInfo windows are displaying nowInfo windows are displaying nowInfo windows are displaying nowInfo windows are displaying now

--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,20 @@ var mapViewModel = function (poiArray) {
   
   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
   var features = map.data.addGeoJson(this.poiArray);
-  
-  console.dir(features);
+
+  //console.dir(features);
   google.maps.event.addDomListener(window, 'load', [map,features]);
   
   var self = this;
 
   self.pois = ko.observableArray(features);
   
-  
+  self.showInfo = function (poi){
+    var infowindow = new google.maps.InfoWindow();
+    console.dir(poi.getGeometry().get());
+    infowindow.setPosition(poi.getGeometry().get())
+    infowindow.open(map);
+  };
   
 };
 
