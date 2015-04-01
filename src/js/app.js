10fad31ab203a3a4d179a9d6eed48bb2e3ebfbdf
Cam Gillespie <gillespie.cam@gmail.com>
2015-04-01 15:31:15 -0600

    Added wiki api call

--- src/js/app.js
+++ src/js/app.js
@@ -5,21 +5,30 @@ var mapViewModel = function (poiArray) {
   var features = map.data.addGeoJson(this.poiArray);
 
   //console.dir(features);
-  google.maps.event.addDomListener(window, 'load', [map,features]);
+
 
   var self = this;
 
   self.pois = ko.observableArray(features);
-
+  google.maps.event.addDomListener(window, 'load', [map]);
   var lastinfowindow;
   self.showInfo = function (poi){
     if (typeof lastinfowindow != 'undefined'){
       lastinfowindow.close();
     };
     var currentlatlng = poi.getGeometry().get();
+    var currentname = poi.getProperty('name');
+    var wiki = $.ajax( {
+      'url': 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&callback=wikiCallback&titles=' + currentname,
+      'dataType': 'jsonp'
+    } );
+
+    //$.ajax({url:'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json|callback&titles=' + name});
+    console.dir(wiki);
     var infowindow = new google.maps.InfoWindow({
       position: currentlatlng,
-      pixelOffset: new google.maps.Size(0,-25)
+      pixelOffset: new google.maps.Size(0,-25),
+      content: currentname
     });
 
     map.panTo(currentlatlng);
